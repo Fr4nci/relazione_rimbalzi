a0e9ce7164b4d9d815f3e624ce6432da8142864a
Fr4nci <francesco291104@gmail.com>
2024-03-04 23:36:45 +0100

    Fine relazione

--- Disperazione_Atto1.py
+++ Disperazione_Atto1.py
@@ -3,18 +3,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
-stream = wave.open("./Audio.wav")
+stream = wave.open("./Disperazione.wav")
 signal = np.frombuffer(stream.readframes(stream.getnframes()), dtype=np.int16)
 t = np.arange(len(signal)) / stream.getframerate()
 
-plt.figure("Rimbalzi pallina")
+plt.figure("Rimbalzi pallina 1")
 plt.plot(t, signal)
 plt.xlabel("Tempo [s]")
-plt.savefig("Grafico_rimbalzo.pdf")
+plt.savefig("Grafico_rimbalzo_1.pdf")
 plt.show()
 g = 9.81
-t_1 = [0.25374149, 1.3378907, 2.1928567, 2.879543, 3.43581, 3.888680, 4.25915, 4.56335, 4.81170, 5.01293]
-t_2 = [0.254240358, 1.34247168, 2.197514,  2.888841,  3.44512, 3.898321, 4.259859, 4.5642629, 4.820339, 5.02143]
+t_1 = [0.5479137, 1.100776, 1.5465075, 1.9081866, 2.200044]
+t_2 = [0.5488884, 1.101692, 1.5475046, 1.9039198, 2.2010431]
 n = []
 frequency = 1/(44.1 * 10**3)
 t_star = []
@@ -48,14 +48,15 @@ plt.grid(which='both', ls='dashed', color='gray')
 plt.xlabel(r'Numero dei rimbalzi')
 plt.ylabel(r'Altezze dei rimbalzi ($m$)')
 plt.title(r"Grafico $n-h(t)$")
-plt.savefig("Grafico_n-h(t).pdf")
-plt.figure("Grafico dei residui")
+plt.savefig("Grafico_n-h(t)_(1).pdf")
+plt.figure("Grafico dei residui_(1)")
 plt.grid()
 plt.errorbar(n, h - expo(n, h0_hat, gamma_hat), yerr=sigma_h, fmt='o')
 plt.axhline(0, color="black", linestyle="--")
-plt.savefig("Grafico_residui.pdf")
+plt.savefig("Grafico_residui_(1).pdf")
 plt.show()
 chi_quadro = 0
 for x in range(len(h)):
     chi_quadro = chi_quadro + ((h[x] - expo(x, h0_hat, gamma_hat))/sigma_h[x])**2
+print(f"Il chi_quadro è {chi_quadro}")
 print(f"Il chi_quadro normalizzato vale {chi_quadro/len(t_star)}")
